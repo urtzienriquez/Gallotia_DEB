3f7f547922132ba266a8532e2b31184267c36b9d
Urtzi_EU <urtzi.enriquez@gmail.com>
2021-05-26 15:07:40 +0200

    unfinished estimation for simonyi (just to check)

--- run_Gallotia_simonyi.m
+++ run_Gallotia_simonyi.m
@@ -5,10 +5,10 @@ pets = {'Gallotia_simonyi'};
 check_my_pet(pets); 
 
 estim_options('default'); 
-estim_options('max_step_number', 5e5); 
+estim_options('max_step_number', 5e2); 
 estim_options('max_fun_evals', 5e3); 
 
-estim_options('pars_init_method', 1); 
+estim_options('pars_init_method', 2); 
 estim_options('results_output', 3); 
 estim_options('method', 'nm'); 
 
