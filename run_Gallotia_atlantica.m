2d61c62feff8e429f0e42c6c7dcd6aa9b39cfe81
Urtzi_EU <urtzi.enriquez@gmail.com>
2021-05-27 02:44:34 +0200

    estim atlantica

--- run_Gallotia_atlantica.m
+++ run_Gallotia_atlantica.m
@@ -5,11 +5,11 @@ pets = {'Gallotia_atlantica'};
 check_my_pet(pets); 
 
 estim_options('default'); 
-estim_options('max_step_number', 5e5); 
+estim_options('max_step_number', 5e2); 
 estim_options('max_fun_evals', 5e3); 
 
-estim_options('pars_init_method', 1); 
+estim_options('pars_init_method', 2); 
 estim_options('results_output', 3); 
-estim_options('method', 'nm'); 
+estim_options('method', 'no'); 
 
 estim_pars; 
