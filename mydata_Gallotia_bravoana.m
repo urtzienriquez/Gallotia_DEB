a3d4a09bd2ae017625e74124c8aefd96cfef3831
Urtzi_EU <urtzi.enriquez@gmail.com>
2021-05-27 02:30:57 +0200

    added bravoana

---
+++ mydata_Gallotia_bravoana.m
@@ -0,0 +1,116 @@
+function [data, auxData, metaData, txtData, weights] = mydata_Gallotia_bravoana
+
+%% set metaData
+metaData.phylum     = 'Chordata'; 
+metaData.class      = 'Reptilia'; 
+metaData.order      = 'Squamata'; 
+metaData.family     = 'Lacertidae';
+metaData.species    = 'Gallotia_bravoana'; 
+metaData.species_en = 'La Gomera Giant Lizard';  
+metaData.ecoCode.climate = {'BWh'};
+metaData.ecoCode.ecozone = {'THp'};
+metaData.ecoCode.habitat = {'0iTg'};
+metaData.ecoCode.embryo  = {'Tt'};
+metaData.ecoCode.migrate = {};
+metaData.ecoCode.food    = {'biCi'};
+metaData.ecoCode.gender  = {'Dg'};
+metaData.ecoCode.reprod  = {'O'};
+metaData.T_typical  = C2K(26); % K, body temp
+metaData.data_0     = {'ab'; 'ap'; 'am'; 'Lb'; 'Lp'; 'Lpm'; 'Li'; 'Lim'; 'Wwi'; 'Wwim'; 'Ri'}; 
+metaData.data_1     = {'L-N'}; 
+
+metaData.COMPLETE = 2.4; % using criteria of LikaKear2011
+
+metaData.author   = {'Urtzi Enriquez-Urzelai'};    
+metaData.date_subm = [2021 05 26];              
+metaData.email    = {'urtzi.enriquez@gmail.com'};            
+metaData.address  = {'Czech Academy of Sciences'};   
+
+metaData.curator     = {'Nina Marn'};
+metaData.email_cur   = {'nina.marn@gmail.com'}; 
+metaData.date_acc    = [2021 05 26];
+
+%% set data
+% zero-variate data
+
+data.ab = 60;  units.ab = 'd';    label.ab = 'age at birth';         bibkey.ab = 'Mate2007';   
+  temp.ab = C2K(24); units.temp.ab = 'K'; label.temp.ab = 'temperature';
+  comment.ab = 'based on Lacerta agilis';
+data.tp = 35*30;     units.tp = 'd';    label.tp = 'time since birth at puberty';  bibkey.tp = 'guess';
+  temp.tp = C2K(24); units.temp.tp = 'K'; label.temp.tp = 'temperature';
+  comment.tp = 'taken from G. simonyi';
+data.am = 133*30;  units.am = 'd';    label.am = 'life span';            bibkey.am = 'guess';   
+  temp.am = C2K(24); units.temp.am = 'K'; label.temp.am = 'temperature'; 
+  comment.am = 'taken from G. simonyi';
+
+data.Lb  = 5.5;    units.Lb  = 'cm';  label.Lb  = 'SVL at hatching';      bibkey.Lb  = 'Mate2007';  
+data.Lp  = 13.1;    units.Lp  = 'cm';  label.Lp  = 'SVL at puberty';      bibkey.Lp  = 'guess';  
+data.Lpm  = 13.5;    units.Lpm  = 'cm';  label.Lpm  = 'SVL at puberty males';      bibkey.Lpm  = 'guess';  
+data.Li  = 15.5;     units.Li  = 'cm';  label.Li  = 'ultimate SVL';         bibkey.Li  = 'Noga2001';  
+data.Lim  = 19;     units.Lim  = 'cm';  label.Lim  = 'ultimate SVL males';         bibkey.Lim  = 'Noga2001';  
+
+data.Ww0 = 6;   units.Ww0 = 'g';  label.Ww0 = 'wet weight of eggs'; bibkey.Ww0 = 'Mate2007';
+  comment.Ww0 = 'Body mass of eggs';
+data.Wwb = 3.5;   units.Wwb = 'g';  label.Wwb = 'wet weight of hatchlings'; bibkey.Wwb = 'Mate2007';
+  comment.Wwb = 'Body mass of hatchlings';
+data.Wwi = 159.2;   units.Wwi = 'g';  label.Wwi = 'ultimate wet weight'; bibkey.Wwi = 'Lope2015';
+  comment.Wwi = 'ultimate body mass of females';
+data.Wwim = 210.5;   units.Wwim = 'g';  label.Wwim = 'ultimate wet males'; bibkey.Wwim = 'Lope2015';
+  comment.Wwim = 'ultimate body mass of males';
+
+data.Ri  = 7/365;   units.Ri  = '#/d'; label.Ri  = 'maximum reprod rate'; bibkey.Ri  = 'Mate2007';   
+  temp.Ri = C2K(24);   units.temp.Ri = 'K'; label.temp.Ri = 'temperature';
+  comment.Ri = 'one clutch per year';
+
+
+%% set weights for all real data
+weights = setweights(data, []);
+weights.tp = 0.5 * weights.tp;
+weights.am = 0.5 * weights.am;
+weights.Lp = 0.1 * weights.Lp;
+weights.Lpm = 0.1 * weights.Lpm;
+
+%% set pseudodata and respective weights
+[data, units, label, weights] = addpseudodata(data, units, label, weights);
+weights.psd.k_J = 0; weights.psd.k = 0.2;
+data.psd.k = 0.3; units.psd.k  = '-'; label.psd.k  = 'maintenance ratio'; 
+
+%% pack auxData and txtData for output
+auxData.temp = temp;
+txtData.units = units;
+txtData.label = label;
+txtData.bibkey = bibkey;
+txtData.comment = comment;
+
+%% Discussion points
+D1 = 'Temperatures are guessed';
+D2 = 'All "guesses" correspond to data taken from G. simonyi';
+metaData.discussion = struct('D1', D1);
+
+%% Links
+metaData.links.id_CoL = '3F6ZJ'; % Cat of Life
+metaData.links.id_EoL = '1263493'; % Ency of Life
+metaData.links.id_Wiki = 'Gallotia_bravoana'; % Wikipedia
+metaData.links.id_ADW = 'Gallotia_bravoana'; % ADW
+metaData.links.id_Taxo = '1685262'; % Taxonomicon
+metaData.links.id_ReptileDB = 'genus=Gallotia&species=bravoana'; % ReptileDB
+
+%% References
+bibkey = 'Kooy2010'; type = 'Book'; bib = [ ...  % used in setting of chemical parameters and pseudodata
+'author = {Kooijman, S.A.L.M.}, ' ...
+'year = {2010}, ' ...
+'title  = {Dynamic Energy Budget theory for metabolic organisation}, ' ...
+'publisher = {Cambridge Univ. Press, Cambridge}, ' ...
+'pages = {Table 4.2 (page 150), 8.1 (page 300)}, ' ...
+'howpublished = {\url{../../../bib/Kooy2010.html}}'];
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+%
+bibkey = 'MoliRodr2004'; type = 'Article'; bib = [ ... 
+'author = {M. Molina-Borja and M.A. Rodriguez-Dominguez}, ' ... 
+'year = {2004}, ' ...
+'title = {Evolution of biometric and life-history traits in lizards (Gallotia) from the Canary Islands}, ' ...
+'journal = {J. Zool. Syst. Evol. Research}, ' ...
+'volume = {42}, ' ...
+'pages = {44-53}'];
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+
